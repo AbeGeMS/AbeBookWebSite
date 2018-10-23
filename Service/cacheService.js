12884aa17ef61ef6d8f2996175a2eb8ff102a806
Abe Ge <yige@microsoft.com>
2018-10-23 22:26:48 +0800

    update code

--- Service/cacheService.js
+++ Service/cacheService.js
@@ -1,20 +1,16 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var Promise = require("bluebird");
-var log4js_1 = require("log4js");
 var bluebird_1 = require("bluebird");
 var CacheService = /** @class */ (function () {
     function CacheService(bookService, redisClient) {
         this._redisAgent = redisClient;
         this._bookService = bookService;
-        this._log = log4js_1.getLogger();
     }
     CacheService.prototype.getBookList = function () {
         var _this = this;
         try {
-            this._log.info("cacheService.getBookList: start to get book");
             return this._redisAgent.SCAN(0, 20).then(function (values) {
-                _this._log.info("cacheService.getBookList: get books count " + values.length);
                 var booksPromise = values.map(function (v) { return _this._bookService.getTitle(v).then(function (title) {
                     return { BookId: v, Name: title };
                 }, function (err) {
--- server.js
+++ server.js
@@ -1,8 +1,27 @@
-var http = require('http');
-
-http.createServer(function (req, res) {
-    
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('Hello, world!');
-    
-}).listen(process.env.PORT || 8080);
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var express = require("express");
+var path = require("path");
+var bookRouter = require("./router/bookRouter");
+var cacheRouter = require("./router/cacheRouter");
+var cookieParser = require("cookie-parser");
+var Demo = /** @class */ (function () {
+    function Demo() {
+    }
+    Demo.start = function () {
+        var app = express();
+        app.use(cookieParser());
+        app.use(express.static(path.join(__dirname, "public")));
+        app.use("/", bookRouter);
+        app.use("/", cacheRouter);
+        app.get("/", function (req, res) {
+            res.sendFile(path.join(__dirname, "./public/page/index.html"));
+        });
+        var server = app.listen(process.env.PORT || 3000, function () {
+            console.log("[" + new Date().toUTCString() + "] Demo is listening port:%s", server.address().port);
+        });
+    };
+    return Demo;
+}());
+exports.Demo = Demo;
+Demo.start();
